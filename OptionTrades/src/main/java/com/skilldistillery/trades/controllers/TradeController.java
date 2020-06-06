4ea77edaefb06658816c711b0fd1592ac155abd1
Ronald (Jeff) Lopez <lopez.j.ronald@gmail.com>
2020-06-06 15:29:38 -0700

    Created @PostMapping in Trade Controller

--- OptionTrades/src/main/java/com/skilldistillery/trades/controllers/TradeController.java
+++ OptionTrades/src/main/java/com/skilldistillery/trades/controllers/TradeController.java
@@ -2,11 +2,14 @@ package com.skilldistillery.trades.controllers;
 
 import java.util.List;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -30,6 +33,22 @@ public class TradeController {
 		Trade trade = tradeService.findById(id);
 		if (trade == null) {
 			response.setStatus(4040);
+		}
+		return trade;
+	}
+	
+	@PostMapping("trades")
+	public Trade addTrade(@RequestBody Trade trade, HttpServletRequest request, HttpServletResponse response) {
+		try {
+			trade = tradeService.create(trade);
+			response.setStatus(201);
+			StringBuffer url = request.getRequestURL();
+			url.append("/").append(trade.getId());
+			response.setHeader("Location",  url.toString());
+		} catch (Exception e) {
+			e.printStackTrace();
+			response.setStatus(400);
+			trade = null;
 		}
 		return trade;
 	}
