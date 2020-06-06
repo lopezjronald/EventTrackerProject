45f5733cc30859b6f879af95ca259bd14492ba09
Ronald (Jeff) Lopez <lopez.j.ronald@gmail.com>
2020-06-06 15:46:12 -0700

    Added PutMapping and DeleteMaping in Controller

--- OptionTrades/src/main/java/com/skilldistillery/trades/controllers/TradeController.java
+++ OptionTrades/src/main/java/com/skilldistillery/trades/controllers/TradeController.java
@@ -6,9 +6,11 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -53,4 +55,65 @@ public class TradeController {
 		return trade;
 	}
 	
+	@PutMapping("trades/{id}")
+	public Trade updateTrade(@PathVariable Integer id, @RequestBody Trade trade, HttpServletResponse response) {
+		try {
+			trade = tradeService.update(trade, id);
+			if(trade == null) {
+				response.setStatus(404);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+			response.setStatus(400);
+			trade = null;
+		}
+		return trade;
+	}
+	
+	@DeleteMapping("trades/{id}")
+	public void deleteFilm(@PathVariable Integer id, HttpServletResponse response) {
+		try {
+			if (tradeService.delete(id)) {
+				response.setStatus(204);
+			} else {
+				response.setStatus(404);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+			response.setStatus(409);
+		}
+	}
+	
+	
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
